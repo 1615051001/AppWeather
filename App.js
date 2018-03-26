30a01d27bac4b8f96f0dd16e347140071fccd0cd
Wandi Septiari <37161475+1615051001@users.noreply.github.com>
2018-03-26 21:48:39 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import Layout from './src/comp/WeatherProject';
-
+import {
+  AppRegistry,
+  StyleSheet,
+  View
+} from 'react-native';
+import Header from './src/Header';
+import Footer from './src/Footer';
+import Weather from './src/Weather';
 
 export default class App extends React.Component {
   render() {
     return (
-
-      <Layout />
-
+      <View style={styles.containerMain}>
+        <Header headerText="Header"/>
+        <Weather/>
+        <Footer footerText="Footer"/>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  containerMain: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: '#E8EAF6'
+  }
+});
+
